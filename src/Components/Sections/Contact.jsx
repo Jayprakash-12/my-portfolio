e49ec8751704ee01af6975599cd8b94c9640346e
Jay Prakash <kushwahjay745393@gmail.com>
2025-09-04 11:46:26 +0530

    add a new repo

---
+++ src/Components/Sections/Contact.jsx
@@ -0,0 +1,54 @@
+import { useState, useRef } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from "@emailjs/browser";
+
+export const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
+    const form = useRef();
+    const SERVICE_ID = "service_gp72lme"
+    const TEMPLATE_ID = "template_ijqiyok"
+    const PUBLIC_KEY = "8k32AbRPbvjWnOiUf"
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(() => {
+            alert("Message sent")
+            setFormData({name: "", email: "", message: ""})
+        }).catch(() =>alert("Oops something went wrong. Please try again"))
+    }
+
+    return(
+        <section id="contact" className="min-h-screen flex items-center justify-center py-20">
+
+        <RevealOnScroll>
+            <div className="px-4 w-150">
+                <h2 className="text-3xl text-center font-bold mb-8  bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">Get In Touch</h2>
+                <form ref={form} onSubmit={handleSubmit} className="space-y-6">
+                    <div className="relative">
+                        <input type="text" name="name" id="name" required value={formData.name} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500/50 focus:bg-blue-500/5" placeholder="Name..." onChange={(e) => setFormData({...formData, name: e.target.value})}/>
+
+                    </div>
+                    <div className="relative">
+                        <input type="email" name="email" id="email" required value={formData.email} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500/50 focus:bg-blue-500/5" placeholder="example@gmail.com" onChange={(e) => setFormData({...formData, email: e.target.value})}/>
+
+                    </div>
+                    <div className="relative">
+                        <textarea name="message" id="message" rows={5} required value={formData.message} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500/50 focus:bg-blue-500/5" placeholder="Your Message..." onChange={(e) => setFormData({...formData, message: e.target.value})}/>
+
+                    </div>
+
+                    <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_115px_rgba(59,130,246,0.4)]">
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </RevealOnScroll>
+
+        </section>
+    )
+}
